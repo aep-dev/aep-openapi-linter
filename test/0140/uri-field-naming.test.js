720ab9c2b5481474db31db692d82fef83a7acc19
DmitryBr <dmitry.brazgovka@doubleverify.com>
2025-03-19 08:54:29 +0100

    Add rules for aep-140 with docs and tests

---
+++ test/0140/uri-field-naming.test.js
@@ -0,0 +1,76 @@
+const { linterForAepRule } = require('../utils');
+require('../matchers');
+
+let linter;
+
+beforeAll(async () => {
+  linter = await linterForAepRule('0140', 'aep-140-uri-field-naming');
+  return linter;
+});
+
+test('aep-140-uri-field-naming should find warning', () => {
+  const oasDoc = {
+    openapi: '3.0.3',
+    paths: {
+      '/test1': {
+        get: {
+          responses: {
+            200: {
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      url: {
+                        type: 'string',
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    }
+  };
+  return linter.run(oasDoc).then((results) => {
+    expect(results.length).toBe(1);
+    expect(results).toContainMatch({
+      path: ['paths', '/test1', 'get', 'responses', '200', 'content',
+        'application/json', 'schema', 'properties', 'url'],
+      message: 'Fields representing URLs or URIs should be named "uri" rather than "url".',
+    });
+  });
+});
+
+test('aep-140-uri-field-naming should find no warnings', () => {
+  const oasDoc = {
+    openapi: '3.0.3',
+    paths: {
+      '/test1': {
+        get: {
+          responses: {
+            200: {
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      uri: {
+                        type: 'string',
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    }
+  };
+  return linter.run(oasDoc).then((results) => {
+    expect(results.length).toBe(0);
+  });
+});
